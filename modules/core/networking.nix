8c35632d1de9a368c95901c3866d7ce2c96f54ea
helixoid <ayushrajmth91@gmail.com>
2025-11-05 22:33:54 +0530

    DNS settings modified

--- modules/core/networking.nix
+++ modules/core/networking.nix
@@ -1,7 +1,10 @@
 {hostname, ...}: {
   networking = {
     hostName = hostname;
-    networkmanager.enable = true;
+    networkmanager = {
+      enable = true;
+      dns = "none";
+    };
     nameservers = ["1.1.1.1" "1.0.0.1"];
     firewall = {
       enable = true;
